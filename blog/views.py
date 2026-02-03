69d304c39287ec48daa0fd71dedff52be8fb5686
Matheus <theushss.dev@gmail.com>
2026-02-02 21:12:26 -0300

    atualizando posts

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,34 @@ from blog.data import posts
 # Create your views here.
 
 def blog(request):
+
+    print("home", id)
     
     context = {
-        'text': '',
+        #'text': '',
         'posts': posts,
     }
     return render(request, 'blog/index.html', context)
+
+def post(request, post_id):
+
+    print("home", id)
+
+    found_post = None
+
+    for post in posts:
+        
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+
+    context = {
+        #'text': '',
+        'post': found_post,
+        'title': found_post['title'] + ' - '
+    }
+    return render(request, 'blog/post.html', context)
 
 def exemplo(request):
     context = {
